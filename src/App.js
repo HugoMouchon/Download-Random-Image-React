f581efad0b06659f6197239002bd9df50709d5d5
Hugo <hugomouchonpro@gmail.com>
2023-03-27 11:30:08 +0200

    Installation de Sass, mise en place du titre et du sous titre

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import style from './App.module.scss';
 
 function App() {
+
+  const [imageList, setImageList] = useState(DATA);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.root}>
+      <h1>Banque d'images</h1>
+      <h2>Télécharge des images open sources alétaoires</h2>
     </div>
   );
 }
 
 export default App;
+
+const DATA = [
+  {
+    "id": "0",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3333,
+    "url": "https://unsplash.com/photos/yC-Yzbqy7PY",
+    "download_url": "https://picsum.photos/id/0/5000/3333"
+  },
+  {
+    "id": "1",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3333,
+    "url": "https://unsplash.com/photos/LNRyGwIJr5c",
+    "download_url": "https://picsum.photos/id/1/5000/3333"
+  },
+  {
+    "id": "2",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3333,
+    "url": "https://unsplash.com/photos/N7XodRrbzS0",
+    "download_url": "https://picsum.photos/id/2/5000/3333"
+  },
+  {
+    "id": "3",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3333,
+    "url": "https://unsplash.com/photos/Dl6jeyfihLk",
+    "download_url": "https://picsum.photos/id/3/5000/3333"
+  },
+  {
+    "id": "4",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3333,
+    "url": "https://unsplash.com/photos/y83Je1OC6Wc",
+    "download_url": "https://picsum.photos/id/4/5000/3333"
+  },
+  {
+    "id": "5",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3334,
+    "url": "https://unsplash.com/photos/LF8gK8-HGSg",
+    "download_url": "https://picsum.photos/id/5/5000/3334"
+  },
+  {
+    "id": "6",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3333,
+    "url": "https://unsplash.com/photos/tAKXap853rY",
+    "download_url": "https://picsum.photos/id/6/5000/3333"
+  },
+  {
+    "id": "7",
+    "author": "Alejandro Escamilla",
+    "width": 4728,
+    "height": 3168,
+    "url": "https://unsplash.com/photos/BbQLHCpVUqA",
+    "download_url": "https://picsum.photos/id/7/4728/3168"
+  },
+  {
+    "id": "8",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3333,
+    "url": "https://unsplash.com/photos/xII7efH1G6o",
+    "download_url": "https://picsum.photos/id/8/5000/3333"
+  },
+  {
+    "id": "9",
+    "author": "Alejandro Escamilla",
+    "width": 5000,
+    "height": 3269,
+    "url": "https://unsplash.com/photos/ABDTiLqDhJA",
+    "download_url": "https://picsum.photos/id/9/5000/3269"
+  }
+]
